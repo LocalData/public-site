aa1249949d00529f99f57c7d9f3ae7884201f36a
Matt Hampel <matthew.hampel@gmail.com>
2013-09-23 23:05:35 -0400

    Auto-adjust photo height based on window height

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 $(function(){
   'use strict';
-  function resize() {
-    $('header .photo').css('min-height', $('.text').height());
-  }
-  window.onresize = resize;
-  resize();
+  // function resize() {
+  //   $('header .photo').css('min-height', $('.text').height());
+  // }
+  // window.onresize = resize;
+  // resize();
+
+  var textHeight = $('.text').height();
+  var height = $( window ).height() - 100;
+  var minHeight = height > textHeight ? height : textHeight;
+  console.log(minHeight);
+  $('header .photo').css('min-height', minHeight);
 });
